2dadef92567bbc50e3840c91565260268a598cb5
caasi Huang <caasi@users.noreply.github.com>
2015-06-13 20:43:17 +0800

    remove `fetchTree`, let the server do the job

--- actions/BuhinActions.js
+++ actions/BuhinActions.js
@@ -3,23 +3,6 @@ import request from 'superagent-bluebird-promise';
 import { assign } from 'lodash';
 
 const server = 'http://chikage.linode.caasih.net';
-const fetchTree = function fetchTree(buhin, onSuccess, onFailed) {
-  for(let op of buhin.data) {
-    switch(op.type) {
-      case 'link':
-        request.get(`${server}/${op.src}.json`)
-          .then(({ body: buhin }) => {
-            op = assign(op, buhin);
-            onSuccess(buhin);
-            fetchTree(buhin, onSuccess, onFailed);
-          })
-          .catch(onFailed);
-        break;
-      default:
-        break;
-    }
-  };
-};
 
 class BuhinActions {
   constructor() {
@@ -28,12 +11,6 @@ class BuhinActions {
   fetch(id) {
     request.get(`${server}/${id}.json`)
       .then(({ body: buhin }) => {
-        fetchTree(
-          buhin,
-          // update the top buhin instead of the fetched one
-          () => this.actions.didFetch(buhin),
-          this.actions.fetchFailed
-        );
         this.actions.didFetch(buhin);
       })
       .catch(this.actions.fetchFailed);
